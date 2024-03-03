48fa856c6bb316180f2a86a8434053c2e96719c2
joaolucasgomesnog <lucasgomes145987@gmail.com>
2024-03-03 18:47:11 -0300

    .

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -35,7 +35,7 @@ export default {
     async findUsuarioById(req, res) {
         try {
             const { id } = req.params
-            const usuario = await prisma.usuario.findMany({ where: { id: Number(id) } })
+            const usuario = await prisma.usuario.findUnique({ where: { id: Number(id) } })
             if (!usuario) return res.json({ error: "usuario não existe" })
             return res.json(usuario)
 
@@ -47,7 +47,7 @@ export default {
     async findUsuarioByEmail(req, res) {
         try {
             const { email } = req.params
-            const usuario = await prisma.usuario.findUnique({ where: { email: email } })
+            const usuario = await prisma.usuario.findMany({ where: { email: email } })
             if (!usuario) return res.json({ error: "usuario não existe" })
             return res.json(usuario)
 
