1b3e77e9508c7821eac65b7102f58a3f03a51d27
joaolucasgomesnog <lucasgomes145987@gmail.com>
2024-03-05 21:46:58 -0300

    .

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -48,8 +48,8 @@ export default {
 
         try {
             const { emailstring } = req.params
-            const emailTratado = emailstring.split('@')
-            const usuario = await prisma.usuario.findMany({where:{email:{startsWith:emailTratado[0]}}})
+            teste = 'lucasgomes145987'
+            const usuario = await prisma.usuario.findMany({where:{email: {contains: teste}}})
 
             if (!usuario) return res.json({ error: "usuario não existe" })
             console.log('Usuario:', usuario)
